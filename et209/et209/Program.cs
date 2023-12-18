bd28e7b751cedd3be8ef057d5af6287ffbc5a734
huytran162005 <huytran162005@gmail.com>
2023-12-18 11:08:59 +0700

    code 1

---
+++ et209/et209/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace et209
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tk hieu bij ao tuong ");
+            Console.ReadLine();
+
+        }
+    }
+}
